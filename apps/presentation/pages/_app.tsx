d2cd8729080329575c294783e3065e61806f3a1f
Brian Espinosa <b@bje.co>
2022-07-22 22:07:22 -0700

    Add Fathom & CI

--- apps/presentation/pages/_app.tsx
+++ apps/presentation/pages/_app.tsx
@@ -1,9 +1,38 @@
 import { AppProps } from 'next/app';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import * as Fathom from 'fathom-client';
 import Head from 'next/head';
 import '@code-hike/mdx/dist/index.css';
 import './styles.css';
 
 function CustomApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Initialize Fathom when the app loads
+    // Example: yourdomain.com
+    //  - Do not include https://
+    //  - This must be an exact match of your domain.
+    //  - If you're using www. for your domain, make sure you include that here.
+    Fathom.load('FZBVRVPL', {
+      url: 'https://who.bje.co/script.js',
+      includedDomains: ['portable-hooks.bje.co'],
+    });
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+    // Record a pageview when route changes
+    router.events.on('routeChangeComplete', onRouteChangeComplete);
+
+    // Unassign event listener (cleanup)
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps -- This empty dependency array is needed to only run on mount
+  }, []);
+
   return (
     <>
       <Head>
