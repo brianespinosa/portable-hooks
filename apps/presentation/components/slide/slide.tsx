797a81d2d10430ecdcdb7eed17b9b4e37f70321f
Brian Espinosa <b@bje.co>
2022-09-10 15:34:04 -0700

    More slide progress

--- apps/presentation/components/slide/slide.tsx
+++ apps/presentation/components/slide/slide.tsx
@@ -6,20 +6,29 @@ import details from '../../details.json';
 const { title, date, event } = details;
 
 export interface SlideProps {
+  columns?: 1 | 2 | 3;
   children?: React.ReactNode;
   hideFooter?: boolean;
   isSmall?: boolean;
+  isTop?: boolean;
 }
 
 export function Slide({
   children,
+  columns,
   hideFooter = false,
   isSmall = false,
+  isTop = false,
 }: SlideProps) {
   return (
     <>
       <motion.section
-        className={clsx(styles._, isSmall && styles.small)}
+        className={clsx(
+          styles._,
+          isSmall && styles.small,
+          isTop && styles.top,
+          columns && styles[`columns-${columns}`]
+        )}
         animate={{ opacity: 1 }}
         initial={{ opacity: isSmall ? 1 : 0 }}
         style={{ zIndex: 10 }} // This is to make sure the first section is always on top
